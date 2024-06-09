efc5adac231b687cdb5ef97704926607017ef2c0
lilyleo7777 <143675794+lilyleo7777@users.noreply.github.com>
2024-06-09 18:46:25 +1000

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain.schema import HumanMessage
 from langchain_pinecone import Pinecone   # Updated import
+from langchain_pinecone import PineconeVectorStore 
 
 # Set up the environment variable for API key
 os.environ["GOOGLE_API_KEY"] = st.secrets["GOOGLE_API_KEY"]
@@ -15,11 +16,11 @@ os.environ["PINECONE_API_KEY"] = st.secrets["PINECONE_API_KEY"]
 genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
 
 # Initialize Pinecone
-pc = Pinecone(api_key=os.environ.get("PINECONE_API_KEY"))
+pinecone.init(api_key=os.environ.get("PINECONE_API_KEY"))
 
 index_name_qa2 = 'adhd-qa2'
 
-index_qa2 = pc.Index(index_name_qa2)
+index_qa2 = pinecone.Index(index_name_qa2)
 time.sleep(1)
 
 #creating vectorstore that holds FAQ doc embeddings 
